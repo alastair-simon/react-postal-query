f83f31212dc54aa6a6642395440b3d0b8e66a638
Alastair Fraser <alastairfraser.work@gmail.com>
2024-06-01 11:51:46 +0200

    file struc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home/Home'
+import Results from './pages/Results/Results'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="bg-red-500">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <Home />
+      <Results/>
     </>
   )
 }
-
-export default App
